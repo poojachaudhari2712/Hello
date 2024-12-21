7b1bbf9b9d99f160e93a2b0ff15adaf2758a151e
Omsamrit <omsamrit19@gmail.com>
2024-12-21 17:21:47 +0530

    updated

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 package Hello;
 public class helloworld{
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World form jharkhand!");
     }
 }
